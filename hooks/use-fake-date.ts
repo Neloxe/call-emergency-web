a07582712de6e0c59bf527d65c17998fc37f06ca
Enzo Sergiani <enzo.sergiani@protonmail.com>
2025-04-20 12:16:40 +0200

    fix: fake date
    fake date real hours and minutes
    
    causing issue on DatePicker in Forecast

--- hooks/use-fake-date.ts
+++ hooks/use-fake-date.ts
@@ -2,21 +2,15 @@ import { useEffect, useState } from "react";
 
 export function useFakeDate(): Date {
   const now = new Date();
-  const initialFakeDate = new Date(`2024-11-01T${now.getHours().toString().padStart(2, "0")}:00:00`);
+  const initialFakeDate = new Date(`2024-11-01T${now.toTimeString().split(" ")[0]}`);
   const [fakeDate, setFakeDate] = useState<Date>(initialFakeDate);
 
   useEffect(() => {
-    const updateFakeDate = () => {
-      const now = new Date();
-      const newFakeDate = new Date(`2024-11-01T${now.getHours().toString().padStart(2, "0")}:00:00`);
-      setFakeDate(newFakeDate);
-    };
-
-    updateFakeDate();
-
     const interval = setInterval(() => {
-      updateFakeDate();
-    }, 60 * 60 * 1000);
+      const now = new Date();
+      const newFakeDate = new Date(`2024-11-01T${now.toTimeString().split(" ")[0]}`);
+      setFakeDate(newFakeDate);
+    }, 1000);
 
     return () => {
       clearInterval(interval);
